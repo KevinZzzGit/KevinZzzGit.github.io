56d8a2b2c722b8f846248a14a7bc65a23940b4fc
KevinZzz <416014688@qq.com>
2023-08-03 11:31:15 +0800

    docs: vue2文档添加

--- src/.vuepress/sidebar/vue.ts
+++ src/.vuepress/sidebar/vue.ts
@@ -5,7 +5,13 @@ export const vueSideBarArr = arraySidebar([
     text: "Vue.js",
     icon: "vue",
     children: [
-      "MVVM框架"
+      "MVVM框架",
+      "Vue2基本使用",
+      "Vue2中的组件通信",
+      "vue-router的基本使用",
+      "VueX的基本使用",
+      "Vue3基本使用",
+      "pinia的基本使用"
     ],
   }
 ]);
